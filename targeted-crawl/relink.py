6225ce691f09d74f59f32528536def12aed247c2
Hieu Hoang <hieuhoang@gmail.com>
2019-03-27 11:05:57 +0000

    delete duplicate links

--- targeted-crawl/relink.py
+++ targeted-crawl/relink.py
@@ -7,9 +7,33 @@ import mysql.connector
 import hashlib
 
 ######################################################################################
-def Relink(mycursor, fromId, toId):
-    pass
-    #sql =
+def Relink(mycursor, docIds, fromId, toId):
+    for docId in docIds:
+        #print("docId", docId, fromId, toId)
+
+        sql = "UPDATE IGNORE link SET url_id = %s WHERE url_id = %s AND document_id = %s"
+        val = (toId, fromId, docId)
+        mycursor.execute(sql, val)
+
+    sql = "DELETE FROM link WHERE url_id = %s"
+    val = (fromId, )
+    mycursor.execute(sql, val)
+
+    sql = "DELETE FROM url WHERE id = %s"
+    val = (fromId, )
+    mycursor.execute(sql, val)
+
+######################################################################################
+def GetDocIds(mycursor):
+    sql = "SELECT id FROM document"
+    mycursor.execute(sql)
+    res = mycursor.fetchall()
+
+    docIds = []
+    for rec in res:
+        docIds.append(rec[0])
+
+    return docIds
 
 ######################################################################################
 
@@ -26,23 +50,31 @@ def Main():
     mydb.autocommit = False
     mycursor = mydb.cursor()
 
+    docIds = GetDocIds(mycursor)
+    print("docIds", len(docIds))
+
     sql = "select t1.id, t2.id" \
         + " from url t1, url t2" \
         + " where right(t1.val, 4) = '.htm'" \
         + " and left(t1.val, length(t1.val) - 4) = t2.val" \
         + " and t1.document_id is null" \
-        + " and t2.document_id is not null"
+        + " and t2.document_id is not null" \
+        + " and t1.id < 1000 and t2.id < 1000"
     mycursor.execute(sql)
     res = mycursor.fetchall()
     print("res", len(res))
 
-    row = res[0]
-    print("row", row)
+    #row = res[0]
+    #print("row", row)
 
-    #for row in res:
-    #    fromId = row[0]
-    #    toId = row[1]
-    #    Relink(fromId, toId)
+    for row in res:
+        print("row", row)
+        fromId = row[0]
+        toId = row[1]
+        Relink(mycursor, docIds, fromId, toId)
+        mydb.commit()
+
+    mydb.commit()
 
     print("Finished")
 
