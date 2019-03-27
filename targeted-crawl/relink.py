3c39610b99b873ac10c0b51982689cea1dc51e50
Hieu Hoang <hieuhoang@gmail.com>
2019-03-27 18:12:54 +0000

    do 1000 at a time

--- targeted-crawl/relink.py
+++ targeted-crawl/relink.py
@@ -53,26 +53,30 @@ def Main():
     docIds = GetDocIds(mycursor)
     print("docIds", len(docIds))
 
-    sql = "select t1.id, t2.id" \
-        + " from url t1, url t2" \
-        + " where right(t1.val, 4) = '.htm'" \
-        + " and left(t1.val, length(t1.val) - 4) = t2.val" \
-        + " and t1.document_id is null" \
-        + " and t2.document_id is not null"
-        #+ " and t1.id < 1000 and t2.id < 1000"
-    mycursor.execute(sql)
-    res = mycursor.fetchall()
-    print("res", len(res))
+    while True:
+        sql = "select t1.id, t2.id" \
+            + " from url t1, url t2" \
+            + " where right(t1.val, 4) = '.htm'" \
+            + " and left(t1.val, length(t1.val) - 4) = t2.val" \
+            + " and t1.document_id is null" \
+            + " and t2.document_id is not null" \
+            + " limit 1000"
+            #+ " and t1.id < 1000 and t2.id < 1000"
+        mycursor.execute(sql)
+        res = mycursor.fetchall()
 
-    #row = res[0]
-    #print("row", row)
+        numRec = len(res)
+        print("res", numRec)
 
-    for row in res:
-        print("row", row)
-        fromId = row[0]
-        toId = row[1]
-        Relink(mycursor, docIds, fromId, toId)
-        mydb.commit()
+        #row = res[0]
+        #print("row", row)
+
+        for row in res:
+            print("row", row)
+            fromId = row[0]
+            toId = row[1]
+            Relink(mycursor, docIds, fromId, toId)
+            mydb.commit()
 
     mydb.commit()
 
