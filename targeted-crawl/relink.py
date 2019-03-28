2e5a603f3aabb9677e60f821c76afb8a50a92c67
Hieu Hoang <hieuhoang@gmail.com>
2019-03-28 14:02:56 +0000

    update all docs in 1 go by only updating those which were null. Avoid duplicate key clash, I think

--- targeted-crawl/relink.py
+++ targeted-crawl/relink.py
@@ -7,13 +7,10 @@ import mysql.connector
 import hashlib
 
 ######################################################################################
-def Relink(mycursor, docIds, fromId, toId):
-    for docId in docIds:
-        #print("docId", docId, fromId, toId)
-
-        sql = "UPDATE IGNORE link SET url_id = %s WHERE url_id = %s AND document_id = %s"
-        val = (toId, fromId, docId)
-        mycursor.execute(sql, val)
+def Relink(mycursor, fromId, toId):
+    sql = "UPDATE link SET url_id = %s WHERE url_id = %s AND document_id IS NULL"
+    val = (toId, fromId)
+    mycursor.execute(sql, val)
 
     sql = "DELETE FROM link WHERE url_id = %s"
     val = (fromId, )
@@ -22,18 +19,6 @@ def Relink(mycursor, docIds, fromId, toId):
     sql = "DELETE FROM url WHERE id = %s"
     val = (fromId, )
     mycursor.execute(sql, val)
-
-######################################################################################
-def GetDocIds(mycursor):
-    sql = "SELECT id FROM document"
-    mycursor.execute(sql)
-    res = mycursor.fetchall()
-
-    docIds = []
-    for rec in res:
-        docIds.append(rec[0])
-
-    return docIds
 
 ######################################################################################
 
@@ -48,39 +33,34 @@ def Main():
         charset='utf8'
     )
     mydb.autocommit = False
-    mycursor = mydb.cursor()
+    mycursor = mydb.cursor(buffered=True)
 
-    docIds = GetDocIds(mycursor)
-    print("docIds", len(docIds))
+    sql = "select t1.id, t2.id" \
+        + " from url t1, url t2" \
+        + " where right(t1.val, 4) = '.htm'" \
+        + " and left(t1.val, length(t1.val) - 4) = t2.val" \
+        + " and t1.document_id is null" \
+        + " and t2.document_id is not null" #\
+        #+ " limit 10"
+        #+ " and t1.id < 1000 and t2.id < 1000"
+    mycursor.execute(sql)
+    res = mycursor.fetchall()
 
-    while True:
-        sql = "select t1.id, t2.id" \
-            + " from url t1, url t2" \
-            + " where right(t1.val, 4) = '.htm'" \
-            + " and left(t1.val, length(t1.val) - 4) = t2.val" \
-            + " and t1.document_id is null" \
-            + " and t2.document_id is not null" \
-            + " limit 1000"
-            #+ " and t1.id < 1000 and t2.id < 1000"
-        mycursor.execute(sql)
-        res = mycursor.fetchall()
+    print("res", len(res))
 
-        numRec = len(res)
-        print("res", numRec)
+    #row = res[0]
+    #print("row", row)
 
-        if numRec == 0:
-            break
-        #row = res[0]
-        #print("row", row)
+    for row in res:
+        print("row", row)
+        fromId = row[0]
+        toId = row[1]
+        Relink(mycursor, fromId, toId)
 
-        for row in res:
-            print("row", row)
-            fromId = row[0]
-            toId = row[1]
-            Relink(mycursor, docIds, fromId, toId)
-            mydb.commit()
-
-    mydb.commit()
+    mycursor.close()
+    mydb.close()
+    mycursor = None
+    mydb = None
 
     print("Finished")
 
