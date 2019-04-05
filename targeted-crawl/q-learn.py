7d19ccd5136ad187f7fbf1db152a830b9bf6f276
Hieu Hoang <hieuhoang@gmail.com>
2019-04-05 16:24:30 +0100

    refactor

--- targeted-crawl/q-learn.py
+++ targeted-crawl/q-learn.py
@@ -3,84 +3,95 @@
 import numpy as np
 import pylab as plt
 
+
 ######################################################################################
 # helpers
 def get_poss_next_states(s, F, ns):
-  poss_next_states = []
-  for j in range(ns):
-    if F[s,j] == 1: poss_next_states.append(j)
-  return poss_next_states
+    poss_next_states = []
+    for j in range(ns):
+        if F[s, j] == 1: poss_next_states.append(j)
+    return poss_next_states
+
 
 def get_rnd_next_state(s, F, ns):
-  poss_next_states = get_poss_next_states(s, F, ns)
-  next_state = \
-    poss_next_states[np.random.randint(0,\
-    len(poss_next_states))]
-  return next_state
+    poss_next_states = get_poss_next_states(s, F, ns)
+    next_state = \
+        poss_next_states[np.random.randint(0, \
+                                           len(poss_next_states))]
+    return next_state
+
 
 def my_print(Q):
-  rows = len(Q); cols = len(Q[0])
-  print("       0      1      2      3      4      5\
+    rows = len(Q);
+    cols = len(Q[0])
+    print("       0      1      2      3      4      5\
       6      7      8      9      10     11     12\
      13     14")
-  for i in range(rows):
-    print("%d " % i, end="")
-    if i < 10: print(" ", end="")
-    for j in range(cols): print(" %6.2f" % Q[i,j], end="")
+    for i in range(rows):
+        print("%d " % i, end="")
+        if i < 10: print(" ", end="")
+        for j in range(cols): print(" %6.2f" % Q[i, j], end="")
+        print("")
     print("")
-  print("")
+
 
 def walk(start, goal, Q):
-  curr = start
-  i = 0
-  print(str(curr) + "->", end="")
-  while curr != goal:
-    next = np.argmax(Q[curr])
-    print(str(next) + "->", end="")
-    curr = next
+    curr = start
+    i = 0
+    print(str(curr) + "->", end="")
+    while curr != goal:
+        next = np.argmax(Q[curr])
+        print(str(next) + "->", end="")
+        curr = next
 
-    i += 1
-    if i > 50:
-        print("LOOPING")
-        break
+        i += 1
+        if i > 50:
+            print("LOOPING")
+            break
 
-  print("done")
+    print("done")
+
 
 ######################################################################################
 
 def train(F, R, Q, gamma, lrn_rate, goal, ns, max_epochs):
-  scores = []
+    scores = []
 
-  for i in range(0,max_epochs):
-    curr_s = np.random.randint(0,ns)  # random start state
+    for i in range(0, max_epochs):
+        curr_s = np.random.randint(0, ns)  # random start state
 
-    while(True):
-      next_s = get_rnd_next_state(curr_s, F, ns)
-      poss_next_next_states = \
-        get_poss_next_states(next_s, F, ns)
+        while (True):
+            next_s = get_rnd_next_state(curr_s, F, ns)
+            poss_next_next_states = \
+                get_poss_next_states(next_s, F, ns)
 
-      max_Q = -9999.99
-      for j in range(len(poss_next_next_states)):
-        nn_s = poss_next_next_states[j]
-        q = Q[next_s,nn_s]
-        if q > max_Q:
-          max_Q = q
+            max_Q = -9999.99
+            for j in range(len(poss_next_next_states)):
+                nn_s = poss_next_next_states[j]
+                q = Q[next_s, nn_s]
+                if q > max_Q:
+                    max_Q = q
 
-      before = Q[curr_s][next_s]
-      # Q = [(1-a) * Q]  +  [a * (rt + (g * maxQ))]
-      Q[curr_s][next_s] = ((1 - lrn_rate) * Q[curr_s] \
-        [next_s]) + (lrn_rate * (R[curr_s][next_s] + \
-        (gamma * max_Q)))
-      after = Q[curr_s][next_s]
-      #print("Q", before, after)
+            before = Q[curr_s][next_s]
+            # Q = [(1-a) * Q]  +  [a * (rt + (g * maxQ))]
+            Q[curr_s][next_s] = ((1 - lrn_rate) * Q[curr_s] \
+                [next_s]) + (lrn_rate * (R[curr_s][next_s] + \
+                                         (gamma * max_Q)))
+            after = Q[curr_s][next_s]
+            # print("Q", before, after)
 
-      curr_s = next_s
-      if curr_s == goal: break
+            curr_s = next_s
+            if curr_s == goal: break
 
-    score = (np.sum(Q / np.max(Q) * 100))
-    scores.append(score)
+        if (np.max(Q) > 0):
+            score = (np.sum(Q / np.max(Q) * 100))
+        else:
+            score = (0)
 
-  return scores
+        scores.append(score)
+
+    return scores
+
 
 ######################################################################################
 
@@ -120,7 +131,7 @@ def Main():
     F[14, 14] = 1
     print("F", F)
 
-    #R = np.random.rand(15, 15)  # Rewards
+    # R = np.random.rand(15, 15)  # Rewards
     MOVE_REWARD = 1
     R = np.zeros(shape=[15, 15], dtype=np.float)  # Rewards
     R[0, 1] = MOVE_REWARD;
@@ -141,7 +152,7 @@ def Main():
     R[8, 3] = MOVE_REWARD
     R[8, 7] = MOVE_REWARD;
     R[9, 4] = MOVE_REWARD;
-    R[9, 14] = 10.0;   # final move
+    R[9, 14] = 10.0;  # final move
     R[10, 5] = MOVE_REWARD
     R[10, 11] = MOVE_REWARD;
     R[11, 10] = MOVE_REWARD;
@@ -163,20 +174,20 @@ def Main():
     ns = 15  # number of states
     gamma = 0.5
     lrn_rate = 0.5
-    max_epochs = 1000
+    max_epochs = 10
     scores = train(F, R, Q, gamma, lrn_rate, goal, ns, max_epochs)
     print("Trained")
 
     print("The Q matrix is: \n ")
     my_print(Q)
 
-    plt.plot(scores)
-    plt.show()
+    #plt.plot(scores)
+    #plt.show()
 
     print("Using Q to go from 0 to goal (14)")
     walk(start, goal, Q)
 
-    #for s in range(0,10):
+    # for s in range(0,10):
     #    nextStates = get_poss_next_states(s, F, ns)
     #    print("nextStates", nextStates)
 
@@ -185,6 +196,6 @@ def Main():
 
     print("Finished")
 
+
 if __name__ == "__main__":
     Main()
-
