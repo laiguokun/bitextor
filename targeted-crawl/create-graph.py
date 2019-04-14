bb0ab49a928aca4ce0c845dd5e3336f36e548145
Hieu Hoang <hieuhoang@gmail.com>
2019-04-14 01:08:16 +0100

    comment out unneeded args

--- targeted-crawl/create-graph.py
+++ targeted-crawl/create-graph.py
@@ -67,9 +67,9 @@ def Main():
 
     oparser = argparse.ArgumentParser(description="create-graph")
     oparser.add_argument("--root-page", dest="rootPage", required=True, help="Starting url of domain")
-    oparser.add_argument('--lang1', dest='l1', help='Language l1 in the crawl', required=True)
-    oparser.add_argument('--lang2', dest='l2', help='Language l2 in the crawl', required=True)
-    oparser.add_argument('--out-file', dest='outFile', help='Output dot file', required=True)
+    #oparser.add_argument('--lang1', dest='l1', help='Language l1 in the crawl', required=True)
+    #oparser.add_argument('--lang2', dest='l2', help='Language l2 in the crawl', required=True)
+    #oparser.add_argument('--out-file', dest='outFile', help='Output dot file', required=True)
 
     options = oparser.parse_args()
 
