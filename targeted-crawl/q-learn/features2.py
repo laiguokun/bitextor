104f31f38c74ad9819229c506528a39c6710bac5
Hieu Hoang <hieuhoang@gmail.com>
2019-07-16 11:50:29 +0100

    debug

--- targeted-crawl/q-learn/features2.py
+++ targeted-crawl/q-learn/features2.py
@@ -291,7 +291,7 @@ class Env:
         assert (res is not None)
 
         for rec in res:
-            print("rec", rec[0], rec[1], rec[2], rec[3])
+            #print("rec", rec[0], rec[1], rec[2], rec[3])
             url = rec[3]
             if url[-10:] == "robots.txt":
                 #print("Skipping robots.txt")
