f87fc053d4b52b35615ace9af6998a778bb6afa5
Hieu Hoang <hieuhoang@gmail.com>
2019-07-25 00:51:01 +0200

    crawl class

--- targeted-crawl/crawl.py
+++ targeted-crawl/crawl.py
@@ -14,56 +14,74 @@ def ConvertEncoding(data, encoding):
     return ''
 
 ######################################################################################
-def FollowLinks(soup):
-    coll = soup.findAll('a')
+class CrawlHost:
+    def __init__(self, url, maxCount):
+        self.url = url
+        self.maxCount = 100
+        self.count = 0
 
-    for link in coll:
-        url = link.get('href')
-        if url is None:
-            continue
-        url = url.strip()
-        
-        linkStr = link.string
-        print("url", linkStr, url)
-        
-        imgURL = link.find('img')
-        if imgURL:
-            # print("imgURL", imgURL)
-            imgURL = imgURL.get('src')
-            if imgURL is not None:
-                imgURL = str(imgURL)
-        else:
-            imgURL = None
+    ######################################################################################
+    def Start(self):
+        self.Download(self.url)
+    
+    ######################################################################################
+    def Download(self, url):
+        self.count += 1
+        if self.count > self.maxCount:
+            return False
 
-######################################################################################
-def Download(url):
-    pageResponse = requests.get(url, timeout=5)
-    print("status_code", pageResponse.status_code)
+        pageResponse = requests.get(url, timeout=5)
+        print("status_code", pageResponse.status_code)
 
-    for histResponse in pageResponse.history:
-        print("   histResponse", histResponse, histResponse.url, histResponse.headers['Content-Type'], \
-                histResponse.apparent_encoding, histResponse.encoding)
-        #print(histResponse.text)
+        for histResponse in pageResponse.history:
+            print("   histResponse", histResponse, histResponse.url, histResponse.headers['Content-Type'], \
+                    histResponse.apparent_encoding, histResponse.encoding)
+            #print(histResponse.text)
 
-    print("pageResponse", pageResponse, pageResponse.url, pageResponse.headers['Content-Type'], \
-            pageResponse.apparent_encoding, pageResponse.encoding)
-    #print(pageResponse.text)
+        print("pageResponse", pageResponse, pageResponse.url, pageResponse.headers['Content-Type'], \
+                pageResponse.apparent_encoding, pageResponse.encoding)
+        #print(pageResponse.text)
 
-    text = pageResponse.text
-    #text = ConvertEncoding(pageResponse.text, pageResponse.encoding)
+        text = pageResponse.text
+        #text = ConvertEncoding(pageResponse.text, pageResponse.encoding)
 
-    content = pageResponse.content
+        content = pageResponse.content
 
-    with open("text", "w") as f:
-        f.write(text)
+        dirName = str(self.count)
+        os.mkdir(dirName)
+        with open(dirName + "/text", "w") as f:
+            f.write(text)
 
-    with open("content", "wb") as f:
-        f.write(content)
+        with open(dirName + "/content", "wb") as f:
+            f.write(content)
 
-    soup = BeautifulSoup(content, features='html5lib') # lxml html.parser
-    #soup = BeautifulSoup(pageResponse.text, features='html5lib') # lxml html.parser
+        soup = BeautifulSoup(content, features='html5lib') # lxml html.parser
+        #soup = BeautifulSoup(pageResponse.text, features='html5lib') # lxml html.parser
 
-    FollowLinks(soup)
+        self.FollowLinks(soup)
+
+    ######################################################################################
+    def FollowLinks(self, soup):
+        coll = soup.findAll('a')
+
+        for link in coll:
+            url = link.get('href')
+            if url is None:
+                continue
+            url = url.strip()
+            
+            linkStr = link.string
+            print("url", linkStr, url)
+            
+            imgURL = link.find('img')
+            if imgURL:
+                # print("imgURL", imgURL)
+                imgURL = imgURL.get('src')
+                if imgURL is not None:
+                    imgURL = str(imgURL)
+            else:
+                imgURL = None
+
 
 ######################################################################################
 
@@ -73,7 +91,8 @@ def Main():
     url = "http://www.visitbritain.com"
     #url = "http://www.buchmann.ch"
     #url = "https://www.buchmann.ch/catalog/default.php"
-    Download(url)
+    crawler = CrawlHost(url, 7)
+    crawler.Start()
 
     print("Finished")
 
