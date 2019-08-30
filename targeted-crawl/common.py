a8407c4f6554f6ed75df6664377f31917fb9617d
Hieu Hoang <hieuhoang@gmail.com>
2019-08-30 22:37:33 +0100

    GetLangId

--- targeted-crawl/common.py
+++ targeted-crawl/common.py
@@ -59,7 +59,11 @@ class Languages:
 
         for res in ress:
             self.coll[res[1]] = res[0]
+            self.maxLangId = res[0]
 
+    def GetLangId(self):
+        return self.maxLangId
+        
     def GetLang(self, str):
         str = StrNone(str)
         if str in self.coll:
