e53d0e786c63f1748f541c7938a0da65798ba9b9
Hieu Hoang <hieuhoang@gmail.com>
2019-09-09 20:27:34 +0100

    refactor

--- targeted-crawl/scratchpad/reinforce-tf3.py
+++ targeted-crawl/scratchpad/reinforce-tf3.py
@@ -1,8 +1,6 @@
 #!/usr/bin/env python3
 #https://github.com/awjuliani/DeepRL-Agents/blob/master/Vanilla-Policy.ipynb?source=post_page-----ded33892c724----------------------
 
-
-
 import tensorflow as tf
 import tensorflow.contrib.slim as slim
 import numpy as np
@@ -14,11 +12,8 @@ try:
 except:
     xrange = range
 
-env = gym.make('CartPole-v0')
-
-gamma = 0.99
-
-def discount_rewards(r):
+######################################################################################
+def discount_rewards(r, gamma):
     """ take 1D float array of rewards and compute discounted reward """
     discounted_r = np.zeros_like(r)
     running_add = 0
@@ -27,6 +22,7 @@ def discount_rewards(r):
         discounted_r[t] = running_add
     return discounted_r
 
+######################################################################################
 class agent():
     def __init__(self, lr, s_size,a_size,h_size):
         #These lines established the feed-forward part of the network. The agent takes a state and produces an action.
@@ -56,64 +52,72 @@ class agent():
         optimizer = tf.train.AdamOptimizer(learning_rate=lr)
         self.update_batch = optimizer.apply_gradients(zip(self.gradient_holders,tvars))
 
-tf.reset_default_graph() #Clear the Tensorflow graph.
+######################################################################################
+def main():
+    env = gym.make('CartPole-v0')
 
-myAgent = agent(lr=1e-2,s_size=4,a_size=2,h_size=8) #Load the agent.
+    gamma = 0.99
 
-total_episodes = 5000 #Set total number of episodes to train agent on.
-max_ep = 999
-update_frequency = 5
+    tf.reset_default_graph() #Clear the Tensorflow graph.
 
-init = tf.global_variables_initializer()
+    myAgent = agent(lr=1e-2,s_size=4,a_size=2,h_size=8) #Load the agent.
 
-# Launch the tensorflow graph
-with tf.Session() as sess:
-    sess.run(init)
-    i = 0
-    total_reward = []
-    total_length = []
-        
-    gradBuffer = sess.run(tf.trainable_variables())
-    for ix,grad in enumerate(gradBuffer):
-        gradBuffer[ix] = grad * 0
-        
-    while i < total_episodes:
-        s = env.reset()
-        running_reward = 0
-        ep_history = []
-        for j in range(max_ep):
-            #Probabilistically pick an action given our network outputs.
-            a_dist = sess.run(myAgent.output,feed_dict={myAgent.state_in:[s]})
-            a = np.random.choice(a_dist[0],p=a_dist[0])
-            a = np.argmax(a_dist == a)
+    total_episodes = 5000 #Set total number of episodes to train agent on.
+    max_ep = 999
+    update_frequency = 5
 
-            s1,r,d,_ = env.step(a) #Get our reward for taking an action given a bandit.
-            ep_history.append([s,a,r,s1])
-            s = s1
-            running_reward += r
-            if d == True:
-                #Update the network.
-                ep_history = np.array(ep_history)
-                ep_history[:,2] = discount_rewards(ep_history[:,2])
-                feed_dict={myAgent.reward_holder:ep_history[:,2],
-                        myAgent.action_holder:ep_history[:,1],myAgent.state_in:np.vstack(ep_history[:,0])}
-                grads = sess.run(myAgent.gradients, feed_dict=feed_dict)
-                for idx,grad in enumerate(grads):
-                    gradBuffer[idx] += grad
+    init = tf.global_variables_initializer()
 
-                if i % update_frequency == 0 and i != 0:
-                    feed_dict= dictionary = dict(zip(myAgent.gradient_holders, gradBuffer))
-                    _ = sess.run(myAgent.update_batch, feed_dict=feed_dict)
-                    for ix,grad in enumerate(gradBuffer):
-                        gradBuffer[ix] = grad * 0
-                
-                total_reward.append(running_reward)
-                total_length.append(j)
-                break
+    # Launch the tensorflow graph
+    with tf.Session() as sess:
+        sess.run(init)
+        i = 0
+        total_reward = []
+        total_length = []
+            
+        gradBuffer = sess.run(tf.trainable_variables())
+        for ix,grad in enumerate(gradBuffer):
+            gradBuffer[ix] = grad * 0
+            
+        while i < total_episodes:
+            s = env.reset()
+            running_reward = 0
+            ep_history = []
+            for j in range(max_ep):
+                #Probabilistically pick an action given our network outputs.
+                a_dist = sess.run(myAgent.output,feed_dict={myAgent.state_in:[s]})
+                a = np.random.choice(a_dist[0],p=a_dist[0])
+                a = np.argmax(a_dist == a)
 
-        
-            #Update our running tally of scores.
-        if i % 100 == 0:
-            print(np.mean(total_reward[-100:]))
-        i += 1
+                s1,r,d,_ = env.step(a) #Get our reward for taking an action given a bandit.
+                ep_history.append([s,a,r,s1])
+                s = s1
+                running_reward += r
+                if d == True:
+                    #Update the network.
+                    ep_history = np.array(ep_history)
+                    ep_history[:,2] = discount_rewards(ep_history[:,2], gamma)
+                    feed_dict={myAgent.reward_holder:ep_history[:,2],
+                            myAgent.action_holder:ep_history[:,1],myAgent.state_in:np.vstack(ep_history[:,0])}
+                    grads = sess.run(myAgent.gradients, feed_dict=feed_dict)
+                    for idx,grad in enumerate(grads):
+                        gradBuffer[idx] += grad
 
+                    if i % update_frequency == 0 and i != 0:
+                        feed_dict= dictionary = dict(zip(myAgent.gradient_holders, gradBuffer))
+                        _ = sess.run(myAgent.update_batch, feed_dict=feed_dict)
+                        for ix,grad in enumerate(gradBuffer):
+                            gradBuffer[ix] = grad * 0
+                    
+                    total_reward.append(running_reward)
+                    total_length.append(j)
+                    break
+
+            
+                #Update our running tally of scores.
+            if i % 100 == 0:
+                print(np.mean(total_reward[-100:]))
+            i += 1
+
+######################################################################################
+main()
