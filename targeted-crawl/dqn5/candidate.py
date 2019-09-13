9c67d663493cbc1c4ae2698bc27fbbe1d2eb9e07
Hieu Hoang <hieuhoang@gmail.com>
2019-09-13 12:28:30 +0100

    get ready to delete lang visited

--- targeted-crawl/dqn5/candidate.py
+++ targeted-crawl/dqn5/candidate.py
@@ -10,13 +10,21 @@ from helpers import GetVistedSiblings, GetMatchedSiblings
 
 ######################################################################################
 def UpdateLangsVisited(langsVisited, node, langIds):
-        if node.lang == langIds[0, 0]:
-            langsVisited[0, 0] += 1
-        elif node.lang == langIds[0, 1]:
-            langsVisited[0, 1] += 1
-        else:
-            langsVisited[0, 2] += 1
+    if node.lang == langIds[0, 0]:
+        langsVisited[0, 0] += 1
+    elif node.lang == langIds[0, 1]:
+        langsVisited[0, 1] += 1
+    else:
+        langsVisited[0, 2] += 1
 
+def GetLangsVisited(visited, node, langIds, env):
+    langsVisited = np.zeros([1, 3]) # langId -> count
+
+    for urlId in visited:
+        node = env.nodes[urlId]
+        UpdateLangsVisited(langsVisited, node, langIds)
+
+    return langsVisited
 ######################################################################################
 class Candidates:
     def __init__(self, params, env):
