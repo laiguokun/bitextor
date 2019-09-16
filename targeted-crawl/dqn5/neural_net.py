b350d0235215ffa8db089a5959cf2dfd3613072e
Hieu Hoang <hieuhoang@gmail.com>
2019-09-16 18:59:36 +0100

    batchable model

--- targeted-crawl/dqn5/neural_net.py
+++ targeted-crawl/dqn5/neural_net.py
@@ -129,28 +129,43 @@ class Qnetwork():
         self.hidden3 = tf.add(self.hidden3, self.b3)
         #self.hidden3 = tf.nn.relu(self.hidden3)
         self.hidden3 = tf.math.sigmoid(self.hidden3)
-        #print("self.hidden3", self.hidden3.shape)
+        print("self.hidden3", self.hidden3.shape)
 
         # link-specific
-        self.hidden3 = tf.transpose(self.hidden3)
-
-        #self.linkLangEmbedding = tf.nn.embedding_lookup(self.embeddings, self.linkLang)
-        #self.linkLangEmbedding = tf.reshape(self.linkLangEmbedding, [self.batchSize * self.params.MAX_NODES, HIDDEN_DIM])
-        #print("self.linkLang", self.linkLang.shape, self.linkLangEmbedding)
+        print("self.linkLang", self.linkLang.shape)
 
         self.WlinkSpecific = tf.Variable(tf.random_uniform([4, HIDDEN_DIM], 0, 0.01))
         self.blinkSpecific = tf.Variable(tf.random_uniform([1, HIDDEN_DIM], 0, 0.01))
 
-        self.linkSpecific = tf.concat([self.linkLang, self.numSiblings, self.numVisitedSiblings, self.numMatchedSiblings], 0)
+        self.linkSpecific = tf.stack([tf.transpose(self.linkLang), 
+                                    tf.transpose(self.numSiblings), 
+                                    tf.transpose(self.numVisitedSiblings),
+                                    tf.transpose(self.numMatchedSiblings)], 0)
+        print("linkSpecific1", self.linkSpecific.shape)
         self.linkSpecific = tf.transpose(self.linkSpecific)
+        print("linkSpecific2", self.linkSpecific.shape,
+            "self.WlinkSpecific", self.WlinkSpecific.shape, 
+            "self.blinkSpecific", self.blinkSpecific.shape,)
+        self.linkSpecific = tf.reshape(self.linkSpecific, [self.batchSize * self.params.MAX_NODES, 4])
+ 
         self.linkSpecific = tf.matmul(self.linkSpecific, self.WlinkSpecific)
         self.linkSpecific = tf.add(self.linkSpecific, self.blinkSpecific)
+        print("linkSpecific3", self.linkSpecific.shape)
+ 
         #self.linkSpecific = tf.nn.relu(self.linkSpecific)
         self.linkSpecific = tf.math.sigmoid(self.linkSpecific)
+        print("linkSpecific4", self.linkSpecific.shape)
 
+        self.linkSpecific = tf.reshape(self.linkSpecific, [self.batchSize, self.params.MAX_NODES, 512])
+        print("linkSpecific5", self.linkSpecific.shape)
+ 
         # final q-values
-        self.hidden3 = tf.matmul(self.linkSpecific, self.hidden3)
-        self.hidden3 = tf.transpose(self.hidden3)
+        self.hidden3 = tf.reshape(self.hidden3, [self.batchSize, 1, HIDDEN_DIM])
+        print("hidden3.1", self.hidden3.shape)
+        self.hidden3 = tf.multiply(self.linkSpecific, self.hidden3)
+        print("hidden3.2", self.hidden3.shape)
+        self.hidden3 = tf.math.reduce_sum(self.hidden3, axis=2)
+        print("hidden3.3", self.hidden3.shape)
 
         self.qValues = tf.boolean_mask(self.hidden3, self.mask, axis=0)
 
