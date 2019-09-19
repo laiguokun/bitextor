605e2d3c4375ac6ee8cb6e207ee44160ec108ec7
Hieu Hoang <hieuhoang@gmail.com>
2019-09-19 18:17:36 +0100

    not vade

--- targeted-crawl/dqn5/main.py
+++ targeted-crawl/dqn5/main.py
@@ -297,8 +297,8 @@ def main():
     if not os.path.exists(options.saveDirPlots): os.makedirs(options.saveDirPlots, exist_ok=True)
 
     print("options.numTrainHosts", options.numTrainHosts)
-    hosts = ["http://vade-retro.fr/"]
-    #hosts = ["http://www.buchmann.ch/", "http://telasmos.org/", "http://tagar.es/"]
+    #hosts = ["http://vade-retro.fr/"]
+    hosts = ["http://www.buchmann.ch/", "http://telasmos.org/", "http://tagar.es/"]
     #hosts = ["http://www.visitbritain.com/"]
 
     #hostsTest = ["http://vade-retro.fr/"]
