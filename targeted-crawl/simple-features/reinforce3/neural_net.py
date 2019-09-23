818b4196c17b524f4babae24b5028965348cb613
Hieu Hoang <hieuhoang@gmail.com>
2019-09-23 16:25:01 +0100

    20 hidden dims

--- targeted-crawl/simple-features/reinforce3/neural_net.py
+++ targeted-crawl/simple-features/reinforce3/neural_net.py
@@ -54,7 +54,7 @@ class Qnetwork():
     def __init__(self, params):
         self.params = params
 
-        HIDDEN_DIM = 4
+        HIDDEN_DIM = 20
 
         # mask
         self.mask = tf.placeholder(shape=[None, self.params.NUM_ACTIONS], dtype=tf.bool)
