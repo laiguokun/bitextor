c55e8500e1579a49150d18336cc7730da5464f4e
Hieu Hoang <hieuhoang@gmail.com>
2019-09-23 15:13:34 +0100

    begin adding link lang

--- targeted-crawl/simple-features/reinforce3/main.py
+++ targeted-crawl/simple-features/reinforce3/main.py
@@ -240,8 +240,8 @@ def main():
 
     print("options.numTrainHosts", options.numTrainHosts)
     #hosts = ["http://vade-retro.fr/"]
-    #hosts = ["http://telasmos.org/"]
-    hosts = ["http://www.buchmann.ch/", "http://telasmos.org/", "http://tagar.es/"]
+    hosts = ["http://telasmos.org/"]
+    #hosts = ["http://www.buchmann.ch/", "http://telasmos.org/", "http://tagar.es/"]
     #hosts = ["http://www.visitbritain.com/"]
 
     #hostsTest = ["http://vade-retro.fr/"]
